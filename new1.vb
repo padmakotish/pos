0418762c4ab80c654ac0a37894630b1339e6e410
padmakotish <padmakotish@gmail.com>
2018-09-04 20:17:13 +0530

    Add files via upload

---
+++ new1.vb
@@ -0,0 +1,14 @@
+Module mod1
+Sub Main()
+Dim number as integer
+Console.Writeline("Enter a Number" )
+Console.Readline()
+if number < 0 then
+Console.Writeline("The number is negative")
+Else if number > 0 then
+Console.Writeline("The number is positive")
+Else if number = 0 then
+Console.Writeline("The number is 0 ")
+End if
+End Main
+End mod1
